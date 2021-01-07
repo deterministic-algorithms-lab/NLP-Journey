b6850dc9fea7cc41cab049ad02ea735b3e7a1b33
jeevesh8 <creativityinczenyoga@gmail.com>
2021-01-07 11:51:10 +0530

    Removed loss from padded position, it distracts model

--- src/model/decoding_algos.py
+++ src/model/decoding_algos.py
@@ -2,14 +2,26 @@ import jax
 import jax.numpy as jnp
 from functools import partial
 
-@partial(jax.jit, static_argnums = (1,2,3))
-def greedy(key, pure_logits_fn, max_length, task, params, initial_token_ids):
+
+def get_eval_fn(pure_logits_fn, task):
+
+    @jax.jit
+    def pure_eval_fn(params, key, token_ids):
+        logits = pure_logits_fn.apply(params, key, 
+                                      token_ids, training=False,
+                                      task=task)
+        return logits
+    
+    return pure_eval_fn        
+
+@partial(jax.jit, static_argnums = (1,2,))
+def greedy(key, pure_logits_eval_fn, max_length, params, initial_token_ids):
     
     token_ids = initial_token_ids
     for i in range(max_length):
         
         key, subkey = jax.random.split(key)
-        logits = pure_logits_fn.apply(params, subkey, token_ids, training=False, task=task)
+        logits = pure_logits_eval_fn(params, subkey, token_ids)
 
         predictions = jnp.argmax(logits[:,i,:], axis=-1)
     
